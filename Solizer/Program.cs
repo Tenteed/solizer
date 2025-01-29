d4c0d1ad412f8c80f79d14a6ad43a2e793d9717d
Tenteed <michall.kulla@gmail.com>
2025-01-29 20:42:48 +0100

    Repository init

---
+++ Solizer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Management;
+
+const string DeviceProperty = "Caption";
+
+List<string> GetAllDevices()
+{
+    var devices =  new List<string>();
+
+    using var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity");
+
+    foreach (var device in searcher.Get())
+    {
+        if (device[DeviceProperty] is not null)
+        {
+            devices.Add(device[DeviceProperty].ToString());
+        }    
+    }
+
+    searcher.Dispose();
+    return devices;
+}
+
+var devices = GetAllDevices();
+devices.ForEach(x => Console.WriteLine(x));
